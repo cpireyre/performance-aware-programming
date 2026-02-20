a0af02dced7aa4f419c94e8274a1fd48064b0008
Colin <cpireyre@student.42.fr>
2026-02-20 10:40:00 +0200

    Tidy up a little bit

--- sim8086/main.c
+++ sim8086/main.c
@@ -3,33 +3,36 @@
  * if some later opcodes break the rules.
 */
 
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
 #include <assert.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "mov.c"
 
 #define MAX_FILE_SIZE_BYTES 4096
-#define MOV_CODE        0b10001000
-#define MOV_IMM_CODE    0b10110000
-#define OP_MASK         0b11111000
 #define MOD_MASK        0b11000000
 #define REG_MASK        0b00111000
 #define REM_MASK        0b00000111
 
-void    printb(u8 byte)
-{
-    char    *xs = "01";
-    i32     i;
+static i32     Open(char *path);
+static i32     Read(i32 fd, u8 *buf, i32 size);
+static void    printb(u8 byte);
+void           run(u8 *program, i32 size);
 
-    i = 7;
-    while (i >= 0)
-    {
-        write(1, xs + ((byte >> i) & 1), 1);
-        i -= 1;
-    }
-    write(1, "\n", 1);
+int main(int argc, char **argv)
+{
+    i32 fd, read_bytes;
+    u8  buf[MAX_FILE_SIZE_BYTES];
+
+    if (argc != 2) return (0);
+    fd = Open(argv[1]);
+    read_bytes = Read(fd, (u8*)&buf, MAX_FILE_SIZE_BYTES);
+    /* Assumes we fully read file */
+    close(fd);
+    run(buf, read_bytes);
+    return (0);
 }
 
 void    run(u8 *program, i32 size)
@@ -192,28 +195,42 @@ void    run(u8 *program, i32 size)
     }
 }
 
-int main(int argc, char **argv)
+static i32 Read(i32 fd, u8 *buf, i32 size)
 {
-    i32 fd, read_bytes;
-    u8  buf[MAX_FILE_SIZE_BYTES];
+    i32 read_bytes;
 
-    if (argc != 2) return (0);
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-    {
-        printf("Error: open\n");
-        return (-1);
-    }
-    read_bytes = read(fd, buf, MAX_FILE_SIZE_BYTES);
+    read_bytes = read(fd, buf, size);
     if (read_bytes < 0)
     {
         printf("Error: read\n");
-        return (-1);
+        exit(1);
     }
-    write(1, "; ", 2);
-    write(1, argv[1], strlen(argv[1]));
-    write(1, "\n\nbits 16\n\n", 11);
-    run(buf, read_bytes);
-    close(fd);
-    return (0);
+    return (read_bytes);
+}
+
+static i32 Open(char *path)
+{
+    i32 fd;
+
+    fd = open(path, O_RDONLY);
+    if (fd == -1)
+    {
+        printf("Error: open\n");
+        exit(1);
+    }
+    return (fd);
+}
+
+static void    printb(u8 byte)
+{
+    char    *xs = "01";
+    i32     i;
+
+    i = 7;
+    while (i >= 0)
+    {
+        write(1, xs + ((byte >> i) & 1), 1);
+        i -= 1;
+    }
+    write(1, "\n", 1);
 }
